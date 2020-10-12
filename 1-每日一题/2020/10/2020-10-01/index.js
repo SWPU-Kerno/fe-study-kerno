8feaae2f0f564a0706d351115057cdb2e2df510c
ATQQ <2604395430@qq.com>
2020-10-12 23:33:04 +0800

    feat: 更新2020-10-01

--- 1-每日一题/2020/10/2020-10-01/index.js
+++ 1-每日一题/2020/10/2020-10-01/index.js
@@ -1,27 +1,3 @@
-# 实现instanceOf
-
-## 问题描述
-实现一个instanceOf函数
-
-要求效果跟instanceof一样
-
-```js
-function instanceOf(a,b){
-    // ...code
-}
-
-xx instanceof XX // true
-instancoOf(xx,XX) // true
-```
-## 题解
-
-根据原型之间的关系可得出
-
-![](https://img.cdn.sugarat.top/mdImg/MTU4NDM2MzA5ODkyOA==584363098928)
-
-### 迭代
-
-```js
 /**
  * 检测构造函数的原型是否在实例的原型链上
  * @param {object} a 
@@ -41,10 +17,7 @@ function instanceOf(a, b) {
         __proto__ = __proto__.__proto__
     }
 }
-```
 
-### 递归
-```js
 /**
  * 检测构造函数的原型是否在实例的原型链上
  * @param {object} a 
@@ -53,13 +26,16 @@ function instanceOf(a, b) {
 function instanceOf(a, b) {
     return a !== null && (a.__proto__ == b.prototype || instanceOf(a.__proto__, b))
 }
-```
 
-### 测试代码
-```js
 console.log(instanceOf([], Array));
 console.log(instanceOf({}, Object));
 console.log(instanceOf(/^$/, RegExp));
 console.log(instanceOf(function () { }, Function));
 console.log(instanceOf([], Function));
-```
+
+function A() {
+
+}
+console.log(instanceOf(new A(), A));
+console.log(instanceOf(new A().__proto__, Function));
+console.log(instanceOf(new A().__proto__.constructor, Function));
